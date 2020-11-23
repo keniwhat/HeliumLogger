01a91028742186620e4818214f13de492a60770c
Ken Carroll <ken@mangofever.com>
2020-11-23 15:30:31 +0000

    Use Kitura projects.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /**
@@ -21,6 +21,9 @@ import PackageDescription
 
 let package = Package(
     name: "HeliumLogger",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -32,7 +35,7 @@ let package = Package(
         // Note: Syntax to allow compatibility with both Swift 4 and Swift 5 projects.
         // See: https://github.com/apple/swift-log#help-i-need-swift-4
         .package(url: "https://github.com/keniwhat/swift-log.git", .branch("master")),
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.0")
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
